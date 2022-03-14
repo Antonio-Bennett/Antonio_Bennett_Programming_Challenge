3c1a4bdabf803206b788083ad443f49589a97583
Antonio Bennett <bennettantonio199@gmail.com>
2022-03-14 01:08:18 -0400

    initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1); ?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <script src="index.js" defer></script>
+</head>
+
+<body>
+  <?php
+  echo "<h1>Hello World</h1>"
+  ?>
+</body>
+
+</html>
